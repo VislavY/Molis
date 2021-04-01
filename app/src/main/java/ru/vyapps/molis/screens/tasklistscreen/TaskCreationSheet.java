c6f04f92cfbad2c661d6701ad4e8d59dc4e0106f
Vladislav <vladislav.yants@gmail.com>
2021-04-01 22:22:58 +0300

    Add TextInputLayout to task creation sheet

--- app/src/main/java/ru/vyapps/molis/screens/tasklistscreen/TaskCreationSheet.java
+++ app/src/main/java/ru/vyapps/molis/screens/tasklistscreen/TaskCreationSheet.java
@@ -12,13 +12,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import ru.vyapps.molis.R;
 import ru.vyapps.molis.screens.tasklistscreen.TaskListPresenter;
 
 public class TaskCreationSheet extends BottomSheetDialogFragment {
 
-    private EditText editTextTaskName;
+    private TextInputLayout textInputLayoutTaskName;
+    private TextInputEditText textInputEditTextTaskName;
     private Button buttonCreateTask;
 
     private TaskListPresenter presenter;
@@ -33,16 +36,19 @@ public class TaskCreationSheet extends BottomSheetDialogFragment {
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
         View view = inflater.inflate(R.layout.sheet_task_creation, container, false);
-        editTextTaskName = view.findViewById(R.id.editTextTaskName);
+        textInputLayoutTaskName = view.findViewById(R.id.textInputLayoutTaskName);
+        textInputEditTextTaskName = view.findViewById(R.id.textInputEditTextTaskName);
         buttonCreateTask = view.findViewById(R.id.buttonCreateTask);
 
         buttonCreateTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String taskName = editTextTaskName.getText().toString().trim();
-                presenter.createTask(taskName);
+//                String taskName = editTextTaskName.getText().toString().trim();
+//                presenter.createTask(taskName);
+//
+//                dismiss();
 
-                dismiss();
+                textInputLayoutTaskName.setError("Ошибка");
             }
         });
 
