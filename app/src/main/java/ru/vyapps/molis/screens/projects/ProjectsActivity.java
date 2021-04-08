a38db72abdeb55debcf9c6b9424869463906b652
Vladislav <vladislav.yants@gmail.com>
2021-04-08 21:45:06 +0300

    Create projects screen

---
+++ app/src/main/java/ru/vyapps/molis/screens/projects/ProjectsActivity.java
@@ -0,0 +1,15 @@
+package ru.vyapps.molis.screens.projects;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.vyapps.molis.R;
+
+public class ProjectsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects);
+    }
+}
