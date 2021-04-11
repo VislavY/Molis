ad3a853082a1a0d732475dc53332c8896ff88854
Vladislav <vladislav.yants@gmail.com>
2021-04-11 23:08:43 +0300

    Add custom onProjectClickListener to projects adapter

--- app/src/main/java/ru/vyapps/molis/screens/projects/adapters/ProjectsAdapter.java
+++ app/src/main/java/ru/vyapps/molis/screens/projects/adapters/ProjectsAdapter.java
@@ -4,33 +4,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckedTextView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import ru.vyapps.molis.R;
 import ru.vyapps.molis.models.pojo.Project;
 
 public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsAdapter.ProjectsViewHolder> {
 
-    private ArrayList<Project> projects;
+    private OnProjectClickListener onProjectClickListener;
+    private List<Project> projects;
 
-    public ProjectsAdapter(ArrayList<Project> projects) {
+    public ProjectsAdapter(List<Project> projects) {
         this.projects = projects;
     }
 
     @NonNull
     @Override
     public ProjectsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_item, parent);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.project_item, parent, false);
         return new ProjectsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProjectsViewHolder holder, int position) {
-        holder.checkedTextViewProjectsName.setText(projects.get(position).getName());
+        Project project = projects.get(position);
+        holder.textViewProjectsName.setText(project.getName());
     }
 
     @Override
@@ -38,14 +42,31 @@ public class ProjectsAdapter extends RecyclerView.Adapter<ProjectsAdapter.Projec
         return projects.size();
     }
 
-    static class ProjectsViewHolder extends RecyclerView.ViewHolder {
+    public void setOnProjectClickListener(OnProjectClickListener l) {
+        this.onProjectClickListener = l;
+    }
 
-        private CheckedTextView checkedTextViewProjectsName;
+    private interface OnProjectClickListener {
+        void onClick();
+    }
+
+    class ProjectsViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewProjectsName;
 
         public ProjectsViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            checkedTextViewProjectsName = itemView.findViewById(R.id.checkedTextViewProjectName);
+            textViewProjectsName = itemView.findViewById(R.id.textViewProjectName);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (onProjectClickListener != null) {
+                        onProjectClickListener.onClick();
+                    }
+                }
+            });
         }
     }
 }
